d44b43aa76ce37f1ee97ec4b0dd270eb8ee1ad77
Mukesh Lomror <54076057+mukeshlomror@users.noreply.github.com>
2023-04-06 01:53:37 +0530

    TicTacToe with java
    simple command line tictactoe with java

---
+++ TicTacToe/Player.java
@@ -0,0 +1,23 @@
+// package TicTacToe;
+
+public class Player {
+    private String name;
+    private char symbol;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public char getSymbol() {
+        return this.symbol;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSymbol(char symbol) {
+        this.symbol = symbol;
+    }
+
+}
